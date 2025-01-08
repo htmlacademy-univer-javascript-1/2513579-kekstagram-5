c15110342fa27120cf13cb8c10cd07aebf993223
Валерия <lera.shilova.05@inbox.ru>
2025-01-09 03:07:48 +0500

    444

--- js/util.js
+++ js/util.js
@@ -1,2 +1,8 @@
-export const createRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+const getRandomArrayElement = (list) => list[getRandomInteger(0, list.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+
+export { getRandomInteger, getRandomArrayElement, isEscapeKey };
