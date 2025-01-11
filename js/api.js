ca4fb9722a1d34487d1d5e61f2e0e4ea257a74b0
Валерия <lera.shilova.05@inbox.ru>
2025-01-11 15:51:47 +0500

    777

---
+++ js/api.js
@@ -0,0 +1,26 @@
+
+const getData = () => fetch('https://29.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((data) => Promise.resolve(data))
+  .catch(() => Promise.reject());
+
+const sendData = (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+
+  return fetch(
+    'https://29.javascript.htmlacademy.pro/kekstagram',
+    {
+      method: 'POST',
+      body: formData
+    })
+    .then((response) => {
+      if (response.ok) {
+        return Promise.resolve();
+      }
+      throw new Error();
+    })
+    .catch(() => Promise.reject());
+};
+
+export { getData, sendData };
